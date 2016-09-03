73faaa92dddbcb5d1adf7ca7b1c9a8dd569257a1
Mark Railton <mark@markrailton.com>
2016-09-03 14:20:19 +0100

    Corrected for PEP8

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, jsonify, redirect
+from flask import Flask, render_template, request, jsonify, redirect
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
 from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required, current_user
@@ -25,13 +25,15 @@ db = SQLAlchemy(app)
 
 # Define models
 roles_users = db.Table('roles_users',
-        db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
-        db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+                       db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+                       db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
 
 class Role(db.Model, RoleMixin):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(80), unique=True)
     description = db.Column(db.String(255))
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -42,6 +44,7 @@ class User(db.Model, UserMixin):
     roles = db.relationship('Role', secondary=roles_users,
                             backref=db.backref('users', lazy='dynamic'))
 
+
 class Link(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     link = db.Column(db.String(16), unique=True)
@@ -50,15 +53,18 @@ class Link(db.Model):
 
     user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
 
+
 # Setup Flask-Security
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 security = Security(app, user_datastore)
+
 
 @app.route('/', methods=['get'])
 @login_required
 def home(page=1):
     links = Link.query.all()
     return render_template('index.html', links=links, loggedin=True)
+
 
 @app.route('/link', methods=['post'])
 @login_required
@@ -67,12 +73,13 @@ def add_link():
         form = request.form
         url = form['url']
         link = link_generator()
-        item = Link(link = link, visits='0', url=url, user_id=current_user.id)
+        item = Link(link=link, visits='0', url=url, user_id=current_user.id)
         db.session.add(item)
         db.session.commit()
         return jsonify(success=True)
     except IntegrityError:
         return jsonify(success=False)
+
 
 @app.route('/link/<int:id>', methods=['delete'])
 @login_required
@@ -82,6 +89,7 @@ def delete_link(id):
     db.session.commit()
     return jsonify(success=True)
 
+
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
 def route(path):
@@ -90,12 +98,15 @@ def route(path):
     db.session.commit()
     return redirect(url.url, code=302)
 
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
+
 def link_generator(size=8, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
+
 
 if __name__ == '__main__':
     app.run()
